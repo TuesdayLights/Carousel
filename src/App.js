10cb98c630fff338bc2380c8b8b4bfd77d76a5a3
Tuesday Lights <tuesdaylightspayment@gmail.com>
2023-10-12 11:56:57 +0530

    2nd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
 import Carousal from './Components/Carousal';
+import Navbar from './Components/Navbar';
+import '../src/Components/styles.css'
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Carousal/>
     </div>
   );
---
+++ src/Components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+
+
+const Navbar = ()=>{
+
+
+
+    return(
+        <div>
+            <div className="navbar">
+                <div>Home</div>
+                <div>About Us</div>
+                <div>Gallery</div>
+                <div>Blog</div>
+                <div>Stories</div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Navbar;
